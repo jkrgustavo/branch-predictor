a0ab56472bae7f55ebb80d12a49343db21a4831f
Gustavo Jokar <jkrgustavo@gmail.com>
2026-04-29 00:27:40 -0500

    loops usually take the branch

--- benchmarks/loop.s
+++ benchmarks/loop.s
@@ -6,36 +6,40 @@ _main:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
 
-    str x19, [sp, #-16]!    // save x19, stack must be 16 byte aligned
+    // x19 - lcv
+    // x20 - # loops
+    stp x19, x20, [sp, #-16]!
+
     mov x19, #0
+    mov x20, #10
 
-loop_test:
-    cmp x19, #10
-loop_branch:
-    bge taken   // break from loop
-    
-// loop body
-not_taken:
+    b loop
+
+loop_print:
     adrp x0, loop_branch@PAGE
     add x0, x0, loop_branch@PAGEOFF
-    mov x1, #0
 
-    bl printTrace
-
-    add x19, x19, #1
-
-    b loop_test
-
-taken:
-    adrp x0, loop_branch@PAGE
-    add x0, x0, loop_branch@PAGEOFF
     mov x1, #1
 
     bl printTrace
 
+loop:
+    add x19, x19, #1
+    cmp x19, x20
+
+loop_branch:
+    blt loop_print
+
 done:
-    ldr x19, [sp], #16      // restore x19 and update sp
-    ldp x29, x30, [sp], #16     // restore fp and lr
+    adrp x0, loop_branch@PAGE
+    add x0, x0, loop_branch@PAGEOFF
+
+    mov x1, #0
+
+    bl printTrace
+
+    ldp x19, x20, [sp], #16
+    ldp x29, x30, [sp], #16
 
     mov w0, #0
 
